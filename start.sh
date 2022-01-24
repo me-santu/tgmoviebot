695029fe67fd1f706ae93bc12e136499dd368d1e
Don <96370825+SflixBots@users.noreply.github.com>
2022-01-24 14:34:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
-cd /tgmoviebot
-echo "Installing Requirements..."
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://git.heroku.com/imdbautofilterbot.git /imdbautofilterbot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /imdbautofilterbot
+fi
+cd /imdbautofilterbot
+pip freeze > requirements.txt
+echo "Starting Bot...."
 python3 bot.py
